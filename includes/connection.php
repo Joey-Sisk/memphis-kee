59985297d3933e85b653b903a57c20d13da6274a
Joey Sisk <jjmsisk@gmail.com>
2021-05-03 14:10:49 -0500

    moved database connection to connection.php file

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,16 @@
 <?php
 
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+
 try {
   $pdo = new PDO("mysql:host=localhost;dbname=content_manager",
     "root",
@@ -10,3 +21,5 @@ try {
   $error = "ERROR! - Connection to DB failed: " . $e->getMessage();
   exit($error);
 }
+
+?>
