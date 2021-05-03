a9aaf99c51ef464bc5de216c0291f0c3b1a8f704
Joey Sisk <jjmsisk@gmail.com>
2021-05-03 14:17:36 -0500

    added db back into index.php file

--- includes/connection.php
+++ includes/connection.php
@@ -1,25 +1,12 @@
 <?php
 
-//Get Heroku ClearDB connection information
-$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-$active_group = 'default';
-$query_builder = TRUE;
-// Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
-// try {
-//   $pdo = new PDO("mysql:host=localhost;dbname=content_manager",
-//     "root",
-//     "root"
-//   );
-//   // echo "Connected!";
-// } catch (PDOException $e) {
-//   $error = "ERROR! - Connection to DB failed: " . $e->getMessage();
-//   exit($error);
-// }
-
-?>
+try {
+  $pdo = new PDO("mysql:host=localhost;dbname=content_manager",
+    "root",
+    "root"
+  );
+  // echo "Connected!";
+} catch (PDOException $e) {
+  $error = "ERROR! - Connection to DB failed: " . $e->getMessage();
+  exit($error);
+}
