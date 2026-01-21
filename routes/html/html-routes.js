b42a61369ed5487a226cc43790977c6a63a697e1
Joey-Sisk <jjmsisk@gmail.com>
2026-01-20 21:15:27 -0600

    Removed the content management pages from the routes

--- routes/html/html-routes.js
+++ routes/html/html-routes.js
@@ -1,32 +1,32 @@
-const router = require('express').Router();
-const path = require('path');
+const router = require("express").Router();
+const path = require("path");
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/tour.html'));
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/tour.html"));
 });
 
-router.get('/cms', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/cms.html'));
+router.get("/presskit", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/presskit.html"));
 });
 
-router.get('/events', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/events.html'));
+router.get("/video", (req, res) => {
+  res.sendFile(path.join(__dirname, "../../public/video.html"));
 });
 
-router.get('/authors', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/author-manager.html'));
-});
+// router.get('/cms', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/cms.html'));
+// });
 
-router.get('/presskit', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/presskit.html'));
-});
+// router.get('/events', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/events.html'));
+// });
+
+// router.get('/authors', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../../public/author-manager.html'));
+// });
 
 // router.get('/tour', (req, res) => {
 //   res.sendFile(path.join(__dirname, '../../public/tour.html'));
 // });
-
-router.get('/video', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../public/video.html'));
-});
 
 module.exports = router;
